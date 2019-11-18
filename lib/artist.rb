d4e799485910b080e7a9a9c12e2fc38c1ead27eb
Carla Holcomb <carla.programmer@gmail.com>
2019-11-18 00:59:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,7 @@ class Artist
   end
 
 def self.find_or_create_by_name(artist_name)
-  found_artist = self.artists.find {|artist| artist.name == artist_name}
+  found_artist = self.all.find {|artist| artist.name == artist_name}
   if found_artist
     found_artist
   else
